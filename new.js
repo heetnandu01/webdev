7a7d5d51978a77c030d7d366fd4760218f3395a7
Heet <heetnandu01@gmail.com>
2025-03-30 22:54:39 +0530

    Javascript fundamentals

---
+++ new.js
@@ -0,0 +1,5 @@
+
+console.log("Hey I am a console")
+console.log("Hey the code is running")
+console.log("Hey I am also running with apis")
+console.log("the code is looking like wow")
---
+++ script.js
@@ -0,0 +1,25 @@
+alert("Hello world");
+
+console.log("Hey I am a console")
+console.log("Hey the code is running")
+console.log("Hey I am also running with apis")
+console.log("the code is looking like wow")
+
+
+var a = prompt("Enter Your Number")
+var isTrue = confirm("Are you sure you want to leave this page and blast Your Computer")
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+
+
+
+console.log("Your Number is " +  a)
+
+document.title = "Hey I am good"
+
+
+document.body.style.backgroundColor = "red"
